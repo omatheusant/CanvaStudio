d8c2bd67dbc8d01fd654527b473cc6a98db05859
Matheus Santana <santalvesmatheus@gmail.com>
2024-02-12 19:22:57 +0000

    feat: liveblocks initial setup

---
+++ app/(root)/tools/editor/page.tsx
@@ -0,0 +1,10 @@
+import { Room } from "./Room";
+import { CollaborativeApp } from "./ColaborativeApp";
+
+export default function Page() {
+  return (
+    <Room>
+      <CollaborativeApp />
+    </Room>
+  );
+}
