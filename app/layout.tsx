51d26d20e08b578678676e4b109dc251861e20d9
Matheus Santana <santalvesmatheus@gmail.com>
2024-02-12 20:02:03 +0000

    config: liveblocks setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,19 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { Metadata } from "next";
+import { Saira } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Canva Studio",
-  description: "A web toolkit for create, edit and publish original designs!",
+  description:
+    "A minamilist web toolkit for create, edit and publish original designs with real-time colaboration!",
 };
+
+const font = Saira({
+  subsets: ["latin"],
+  variable: "--font-saira",
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 export default function RootLayout({
   children,
@@ -15,7 +23,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${font.className} bg-primary-grey-100`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
